1279b0050febf7c714c42de88755081aa76465f0
Yash Yadav <yash92328@gmail.com>
2024-11-21 18:50:22 +0530

    code improved and added assertion in story titles

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Takes in a story title, number of parts and subparts to generate. And generate an unheard story within minutes.
 
 Model used - gpt-4o-mini
+
+Generates around 500 words for every subpart. Around 12,000 words curated story in 150 seconds.
+
+### How to start
+1. Enter OPENAI_API_KEY in .env_template file and change the file name to .env
+2. Enter titles, # of parts, # of subparts in the last cell of gen.ipynb notebook and run all cells
--- src/prompt.py
+++ src/prompt.py
@@ -1,5 +1,5 @@
 STORY_HIGHLIGHTS_GENERATOR="""
-You are provided with a title or a theme for a hypothetical story. Your task for now is to generate the story highlights from start to end in N parts. Use hypothetical character names, place, time etc. Those parts will be used further to complete the story. Keep the language simple.
+You are provided with a title or a theme for a hypothetical story. Your task for now is to generate the story highlights from start to end in N parts. Use hypothetical character names, place, time etc. Those parts will be used further to complete the story. Keep the language simple. Titles can have any complex form.
 
 You will be provided input as below - 
 
@@ -21,7 +21,7 @@ Only Generate response in JSON format and nothing else.
 
 
 STORY_SUBPARTS_GENERATOR="""
-We are creating a hypothetical story using hypothetical character names, place, time etc. You are provided with the summary of the story and a part's gist on which you need to work on. Your task is to add in more details into the assigned part, and generate N more subparts highlights. Those subparts will be used to complete the story. Keep the language simple.
+We are creating a hypothetical story using hypothetical character names, place, time etc. You are provided with the summary of the story and a part's gist on which you need to work on. Your task is to add in more details into the assigned part, and generate N more subparts highlights. Those subparts will be used to complete the story. Keep the language simple. Titles can have any complex form.
 
 You will be provided input as below - 
 ```
@@ -49,7 +49,7 @@ Only Generate response in JSON format and nothing else.
 
 
 STORY_SUBPARTS_DESCRIPTION_GENERATOR="""
-We are creating a very big hypothetical story using hypothetical character names, place, time etc. You are provided with the summary of the story, summary of a part of the story and a subpart of the part's gist which you need to work on. Your task is to add in more details into the assigned subpart, and complete this subpart as comprehensively as possible. Add in several little details, add more hypothetical things as you wish. Keep the language simple.
+We are creating a very big hypothetical story using hypothetical character names, place, time etc. You are provided with the summary of the story, summary of a part of the story and a subpart of the part's gist which you need to work on. Your task is to add in more details into the assigned subpart, and complete this subpart as comprehensively as possible. Add in several little details, add more hypothetical things as you wish. Keep the language simple. Titles can have any complex form.
 
 You will be provided input as below - 
 ```
